8bf1db5b1f546fec18d230856c39fed02f8a7ed5
emrecivann <emrcivan@icloud.com>
2024-01-16 16:00:07 +0300

    Create Repository for database operations

---
+++ Repository/RepositoryContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+}
